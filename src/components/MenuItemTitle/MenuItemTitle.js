df073410e79fe9e5729ad634958fc941293121b2
Alexandr <alexandr.mirz12@gmail.com>
2021-03-21 23:11:36 +0400

    rendering menu item titles

---
+++ src/components/MenuItemTitle/MenuItemTitle.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const MenuItemTitle = ({ title }) => <h2>{title}</h2>;
+
+export default MenuItemTitle;
--- src/screens/Menu/Menu.js
+++ src/screens/Menu/Menu.js
@@ -10,58 +10,74 @@ import tawouk from '../../assets/images/tawouk.jpeg';
 import lconacSmbuk from '../../assets/images/lconac_smbuk.jpeg';
 
 import './_style.scss';
+import MenuItemTitle from '../../components/MenuItemTitle/MenuItemTitle';
 import MenuItem from '../../components/MenuItem/MenuItem';
 
-const menuItemsList = [
-  {
-    image: amarayin,
-    itemName: 'Ամառային աղցան',
-    itemRecipe: '220գ, լոլիկ, վարունգ, մաղադանոս',
-    itemPrice: 1200,
+const filteredMenuItemsList = {
+  salads: {
+    label: 'Աղցաններ',
+    data: [
+      {
+        image: amarayin,
+        itemName: 'Ամառային աղցան',
+        itemRecipe: '220գ, լոլիկ, վարունգ, մաղադանոս',
+        itemPrice: 1200,
+      },
+      {
+        image: tabule,
+        itemName: 'Աղցան Թաբուլե',
+        itemRecipe: '220գ, բլղուր, հազար, լոլիկ, վարունգ',
+        itemPrice: 1100,
+      },
+      {
+        image: hummus,
+        itemName: 'Հումմուս',
+        itemRecipe: '220գ, սիսեռ, թահինի սոուս, ձեթ',
+        itemPrice: 1000,
+      },
+    ],
   },
-  {
-    image: tabule,
-    itemName: 'Աղցան Թաբուլե',
-    itemRecipe: '220գ, բլղուր, հազար, լոլիկ, վարունգ',
-    itemPrice: 1100,
+  doughDishes: {
+    label: 'Խմորով ուտեստներ',
+    data: [
+      {
+        image: lahmajoAngus,
+        itemName: 'Լահմաջո անգուսի մսով',
+        itemRecipe: '180գ, 38x24սմ, անգուս տավարի',
+        itemPrice: 1100,
+      },
+      {
+        image: lahmajoSpicy,
+        itemName: 'Լահմաջո կծու Ուրֆա',
+        itemRecipe: '180գ, 38x24սմ, գառան միս, մաղադանոս',
+        itemPrice: 1300,
+      },
+      {
+        image: lahmajoGrenade,
+        itemName: 'Լահմաջո նռան օշարակով',
+        itemRecipe: '180գ, 38x24սմ, գառան միս, մաղադանոս',
+        itemPrice: 1400,
+      },
+    ],
   },
-  {
-    image: hummus,
-    itemName: 'Հումմուս',
-    itemRecipe: '220գ, սիսեռ, թահինի սոուս, ձեթ',
-    itemPrice: 1000,
+  mainDishes: {
+    label: 'Հիմնական ուտեստներ',
+    data: [
+      {
+        image: tawouk,
+        itemName: 'Շիշ թաուկ',
+        itemRecipe: '4-5 կտոր, 300գ, հավի փափկամիս',
+        itemPrice: 2200,
+      },
+      {
+        image: lconacSmbuk,
+        itemName: 'Լցոնած սմբուկ',
+        itemRecipe: '4-5 կտոր, 300գ, հավի փափկամիս',
+        itemPrice: 3200,
+      },
+    ],
   },
-  {
-    image: lahmajoAngus,
-    itemName: 'Լահմաջո անգուսի մսով',
-    itemRecipe: '180գ, 38x24սմ, անգուս տավարի',
-    itemPrice: 1100,
-  },
-  {
-    image: lahmajoSpicy,
-    itemName: 'Լահմաջո կծու Ուրֆա',
-    itemRecipe: '180գ, 38x24սմ, գառան միս, մաղադանոս',
-    itemPrice: 1300,
-  },
-  {
-    image: lahmajoGrenade,
-    itemName: 'Լահմաջո նռան օշարակով',
-    itemRecipe: '180գ, 38x24սմ, գառան միս, մաղադանոս',
-    itemPrice: 1400,
-  },
-  {
-    image: tawouk,
-    itemName: 'Շիշ թաուկ',
-    itemRecipe: '4-5 կտոր, 300գ, հավի փափկամիս',
-    itemPrice: 2200,
-  },
-  {
-    image: lconacSmbuk,
-    itemName: 'Լցոնած սմբուկ',
-    itemRecipe: '4-5 կտոր, 300գ, հավի փափկամիս',
-    itemPrice: 3200,
-  },
-];
+};
 
 const Menu = (props) => {
   const entityName = props.match.params.name;
@@ -71,14 +87,21 @@ const Menu = (props) => {
     <div className='menu-screen-wrapper'>
       <div className='d-flex flex-column justify-content-center align-items-center menu-item-row-wrapper'>
         {
-          menuItemsList.map((item, index) => (
-            <MenuItem
-              id={index}
-              image={item.image}
-              itemName={item.itemName}
-              itemRecipe={item.itemRecipe}
-              itemPrice={item.itemPrice}
-            />
+          Object.keys(filteredMenuItemsList).map((menuItemKey) => (
+            <>
+              <MenuItemTitle title={filteredMenuItemsList[menuItemKey].label} />
+              {
+                filteredMenuItemsList[menuItemKey].data.map((menuItems, index) => (
+                  <MenuItem
+                    id={index}
+                    image={menuItems.image}
+                    itemName={menuItems.itemName}
+                    itemRecipe={menuItems.itemRecipe}
+                    itemPrice={menuItems.itemPrice}
+                  />
+                ))
+              }
+            </>
           ))
         }
       </div>
